8af8234209bef4f89fe26e2b416b691ed60e0836
James Richmond <devenger@gmail.com>
2015-06-07 20:51:11 +0100

    Initial commit

---
+++ src/ts/Config.ts
@@ -0,0 +1,25 @@
+/// <reference path="../../lib/def/require.d.ts" />
+
+require.config({
+    baseUrl: "./ts",
+    paths: {
+        lib: "../../lib"
+    }
+});
+
+require(
+    ["lib/p2"],
+    (p2) => {
+        require(
+            ["lib/phaser"],
+            (Phaser) => {
+                require(
+                    ["Main"],
+                    (Main) => {
+                        new Main().run();
+                    }
+                );
+            }
+        );
+    }
+);
---
+++ src/ts/Main.ts
@@ -0,0 +1,31 @@
+/// <reference path="../../lib/def/phaser.d.ts"/>
+/// <reference path="../../lib/def/require.d.ts"/>
+
+class Main {
+    run() {
+        var game:Phaser.Game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
+            preload: preload,
+            create: create,
+            update: update,
+            render: render
+        });
+
+        function preload() {
+            console.log("Preloading...");
+        }
+
+        function create() {
+            console.log("Initialising...");
+        }
+
+        function update() {
+            console.log("Updating...");
+        }
+
+        function render() {
+            console.log("Rendering...");
+        }
+    }
+}
+
+export = Main;

f5306538fcb7774e38330bac2ffce512c9d3eba7
James Richmond <devenger@gmail.com>
2015-11-08 11:47:21 +0000

    updated jsconfig.json and all import statements (for TypeScript 1.6)

--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -1,30 +1,30 @@
 /// <reference path="../../lib/def/phaser.d.ts"/>
 
 class Main {
-    run() {
-        var game:Phaser.Game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
-            preload: preload,
-            create: create,
-            update: update,
-            render: render
-        });
+  run() {
+    var game: Phaser.Game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
+      preload: preload,
+      create: create,
+      update: update,
+      render: render
+    });
 
-        function preload() {
-            console.log("Preloading...");
-        }
-
-        function create() {
-            console.log("Initialising...");
-        }
-
-        function update() {
-            console.log("Updating...");
-        }
-
-        function render() {
-            console.log("Rendering...");
-        }
+    function preload() {
+      console.log("Preloading...");
     }
+
+    function create() {
+      console.log("Initialising...");
+    }
+
+    function update() {
+      console.log("Updating...");
+    }
+
+    function render() {
+      console.log("Rendering...");
+    }
+  }
 }
 
 export = Main;

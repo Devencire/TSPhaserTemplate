ad3d520419ae4db062b0c54aa2d801888d341274
James Richmond <devenger@gmail.com>
2015-11-08 14:24:56 +0000

    changed to having a PlayState instead of loose functions, to aid immediate scalability

--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -1,29 +1,15 @@
 /// <reference path="../../lib/def/phaser.d.ts"/>
 
+import PlayState = require('./PlayState')
+
 class Main {
+  game: Phaser.Game;
+
   run() {
-    var game: Phaser.Game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
-      preload: preload,
-      create: create,
-      update: update,
-      render: render
-    });
+    this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content');
 
-    function preload() {
-      console.log("Preloading...");
-    }
-
-    function create() {
-      console.log("Initialising...");
-    }
-
-    function update() {
-      console.log("Updating...");
-    }
-
-    function render() {
-      console.log("Rendering...");
-    }
+    this.game.state.add("PlayState", PlayState, false);
+    this.game.state.start("PlayState", true, true);
   }
 }
 
---
+++ src/ts/PlayState.ts
@@ -0,0 +1,16 @@
+class PlayState extends Phaser.State {
+  preload() {
+    console.log("Preloading...");
+  }
+  create() {
+    console.log("Initialising...");
+  }
+  update() {
+    console.log("Updating...");
+  }
+  render() {
+    console.log("Rendering...");
+  }
+}
+
+export = PlayState
